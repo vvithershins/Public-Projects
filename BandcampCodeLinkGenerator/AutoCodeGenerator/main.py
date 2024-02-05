df1b3f17cc7eeb42e882f886653e72d7219088d2
vvithershins <146913240+vvithershins@users.noreply.github.com>
2024-02-05 17:29:55 -0500

    Update main.py
    Edited so that it also generates a new file with the completed track/album codes for download purposes and easier copying than from the console.

--- BandcampCodeLinkGenerator/AutoCodeGenerator/main.py
+++ BandcampCodeLinkGenerator/AutoCodeGenerator/main.py
@@ -1,9 +1,14 @@
 codes = []
+
+newcodes = []
+
 print("Bandcamp link generator.")
+
 print("Please put your codes in the codes.txt file, one per line ")
+
 page = input("""enter the name of your band camp page
 https://name.bandcamp.com
-""")
+or just press enter to skip this part.""")
 
 f = open("codes.txt", "r")
 for line in f: 
@@ -15,7 +20,16 @@ for code in codes:
     template = f"https://{page}.bandcamp.com/yum?code={code}"
     print(template)
     print()
+    newcodes.append(template)
+    
   else:
     template = f"https://bandcamp.com/yum?code={code}"
     print(template)
     print()
+    newcodes.append(template)
+    
+f = open("newcodes.txt" , "a+")
+for code in newcodes:
+  code = f"{code}\n"
+  f.write(code)
+f.close()
