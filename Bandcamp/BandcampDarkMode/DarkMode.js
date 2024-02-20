25fc119dc8607d94c1ef99b874c1200fec21147d
vvithershins <146913240+vvithershins@users.noreply.github.com>
2024-02-20 04:04:54 +0000

    02/19/2024 Organization and addition of "Dark Bandcamp Theme"

---
+++ Bandcamp/BandcampDarkMode/DarkMode.js
@@ -0,0 +1,5 @@
+document.getElementById("grids").style.backgroundColor = "black";
+document.querySelector('.carousel-player-inner').style.backgroundColor = 'black';
+document.getElementById("menubar").style.backgroundColor = "black";
+document.querySelector('.top').style.backgroundColor = 'black';
+document.getElementById("menubar-vm").style.backgroundColor = "black";
